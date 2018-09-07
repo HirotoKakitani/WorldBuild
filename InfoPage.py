ebd17f33c79db3a1ef133d2524ff23a1e61a0087
Hiroto Kakitani <hirotokakitani@gmail.com>
2018-09-06 18:36:53 -0700

    added edit feature. Does not save blank titles

--- InfoPage.py
+++ InfoPage.py
@@ -11,22 +11,26 @@ class InfoPage(Frame):
         self.prevSelect = prevSelect    #points to the selection page this entry came from
         self.nameHistory = []
  
-        self.title = Entry(self)
+        self.title = Entry(self, disabledforeground="black")
         self.title.pack(pady=10, padx=10)
+        self.title.config(state=DISABLED)
         self.backButton = Button(self, text="Back", command=lambda:controller.showFrame(INFO))
         self.backButton.pack(pady=10,padx=10) 
         
         self.saveButton = Button(self,text="Save", command=self.save)
         self.saveButton.pack(pady=10, padx=10)
         
+        self.editButton = Button(self, text="Edit", command=self.edit)
+        self.editButton.pack(pady=10, padx=10)
+        
         #TODO text area testing
         
         #TODO-=-= image insertion test
-        photo=PhotoImage(file='testimg.gif')
-        self.textP = Text(root, height=20,width=20)
-        self.textP.insert(END,'\n')
-        self.textP.image_create(END,image=photo)
-        self.textP.pack()
+        #photo=PhotoImage(file='testimg.gif')
+        #self.textP = Text(root, height=20,width=20)
+        #self.textP.insert(END,'\n')
+        #self.textP.image_create(END,image=photo)
+        #self.textP.pack()
         
         self.scrollbar = Scrollbar(self)
         self.textArea = Text(self,height=2)
@@ -35,11 +39,19 @@ class InfoPage(Frame):
         self.scrollbar.config(command=self.textArea.yview)
         self.textArea.config(yscrollcommand=self.scrollbar.set)
         self.textArea.insert(END,"testing text widget\nhello thgis is a new line\nwaaaaaaah\nhello")
-        
-        
+        self.textArea.config(state=DISABLED)
+    
+    #activates text entry areas
+    def edit(self):
+        self.textArea.config(state=NORMAL)
+        self.title.config(state=NORMAL)
+
     def save(self):
+        self.textArea.config(state=DISABLED)
+        self.title.config(state=DISABLED)
         newTitle = self.title.get()
-        if newTitle in self.nameHistory:
+
+        if newTitle in self.nameHistory or not newTitle:
             print "Name has not changed"
 
         elif len(self.nameHistory) == 0:
